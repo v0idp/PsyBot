1ebbb11a4ff6ae1688f0e668efa88872caed5cf0
shrams <shrams@yopmail.com>
2020-04-12 13:35:14 -0400

    use cache (DJS v12 update)

--- src/events/guildCreate.js
+++ src/events/guildCreate.js
@@ -1,4 +1,4 @@
 exports.run = (client, guild) => {
   client.user.setAFK(true);
-  client.user.setActivity(`Running on ${client.guilds.size} servers.`, {type: 'PLAYING'});
+  client.user.setActivity(`Running on ${client.guilds.cache.size} servers.`, {type: 'PLAYING'});
 }
--- src/events/ready.js
+++ src/events/ready.js
@@ -1,6 +1,6 @@
 exports.run = (client) => {
     console.log(`Bot ready and logged in as ${client.user.tag} (${client.user.id}). Prefix set to ${client.commandPrefix}`);
     client.user.setAFK(true);
-    client.user.setActivity(`Running on ${client.guilds.size} servers.`, {type: 'PLAYING'});
+    client.user.setActivity(`Running on ${client.guilds.cache.size} servers.`, {type: 'PLAYING'});
     client.isReady = true;
 }
